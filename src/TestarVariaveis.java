2af1c2d875318b5e0f8181b45b6e39f60c5f4e5e
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2023-07-09 16:43:39 -0300

    float

--- src/TestarVariaveis.java
+++ src/TestarVariaveis.java
@@ -12,6 +12,18 @@ public class TestarVariaveis {
 		//decimal
 		double salario = 7650.70;
 		System.out.println("Meu salário é: " + salario);
+		
+		//divisao decimal
+		double test = 5.0 / 2;
+		System.out.println("test: " + test);
+		
+		//Numero grande
+		long numeroGrande = 23265544556L;
+		System.out.println("test: " + numeroGrande);
+		
+		//Float
+		float numeroTeste = 3.14F; //ou (float) 3.14
+		System.out.println("test: " + numeroTeste);
 	}
 
 }
