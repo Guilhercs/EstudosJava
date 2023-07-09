7b2e3226db4e24a46ac3f2aa2025e5310e1cb62d
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2023-07-08 23:42:19 -0300

    chore(estudos): variaveis

---
+++ src/TestarVariaveis.java
@@ -0,0 +1,17 @@
+
+public class TestarVariaveis {
+	
+	public static void main(String[] args) {
+		//string
+		System.out.println("Hello new test");
+		
+		//inteiro
+		int idade = 30;	
+		System.out.println("Minha idade é " + idade);
+		
+		//decimal
+		double salario = 7650.70;
+		System.out.println("Meu salário é: " + salario);
+	}
+
+}
